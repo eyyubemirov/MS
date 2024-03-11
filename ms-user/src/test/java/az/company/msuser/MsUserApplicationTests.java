08be0902188cc9b355ba9d84578221f2533f5cf0
Eyyub Emirov <eyyubemirov9a@gmail.com>
2024-03-11 17:07:54 +0400

    first commit

---
+++ ms-user/src/test/java/az/company/msuser/MsUserApplicationTests.java
@@ -0,0 +1,15 @@
+package az.company.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootTest
+@EnableEurekaClient
+class MsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
